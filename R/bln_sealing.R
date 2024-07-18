8463f83c5e274a454221d74f77074e4dfc0516fc
Gerard <gerard.ros@nmi-agro.nl>
2024-07-19 02:40:05 +0300

    add bln_p_sealing

---
+++ R/bln_sealing.R
@@ -0,0 +1,38 @@
+#' Calculate and evaluate the soil sealing risk
+#'
+#' This function calculates the risks of soil sealing.
+#'
+#' @param A_SOM_LOI (numeric) The organic matter content of soil (\%)
+#' @param A_CLAY_MI (numeric) The clay content of the soil (\%)
+#'
+#' @import data.table
+#' @import OBIC
+#'
+#' @return
+#' The risk of soil sealing as affected by the soil organic matter and clay content. A numeric value.
+#'
+#' @export
+bln_p_sealing <- function(A_SOM_LOI, A_CLAY_MI) {
+
+  # Check input
+  arg.length <- max(length(A_CLAY_MI), length(A_SOM_LOI))
+  checkmate::assert_numeric(A_CLAY_MI, lower = 0, upper = 100, any.missing = FALSE, min.len = 1, len = arg.length)
+  checkmate::assert_numeric(A_SOM_LOI, lower = 0, upper = 100, any.missing = FALSE, min.len = 1, len = arg.length)
+
+  # Setup a table with all the information
+  dt <- data.table(A_CLAY_MI = A_CLAY_MI,
+                   A_SOM_LOI = A_SOM_LOI
+                  )
+
+  # Calculate the risk of soil surface sealing
+  dt[, D_SE := OBIC::calc_sealing_risk(A_SOM_LOI, A_CLAY_MI)]
+
+  # Evaluates the risk of soil sealing
+  dt[, i_p_se := OBIC::ind_sealing(D_SE, B_LU_BRP)]
+
+  # extract the BLN indicator
+  value <- dt[, i_p_se]
+
+  # return the value
+  return(value)
+}
