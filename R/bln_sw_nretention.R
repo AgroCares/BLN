1179c55248ea146fbc8697e4a01b795efa84d29b
Gerard <gerard.ros@nmi-agro.nl>
2024-11-05 12:00:43 +0100

    add CN ratio to bln_wat_nretention_sw

--- R/bln_sw_nretention.R
+++ R/bln_sw_nretention.R
@@ -9,15 +9,16 @@
 #' @param B_GWL_CLASS (character) The groundwater table class
 #' @param A_SOM_LOI (numeric) The organic matter content of the soil (\%)
 #' @param A_N_RT (numeric) The organic nitrogen content of the soil in mg N / kg
+#' @param A_CN_FR (numeric) The carbon to nitrogen ratio (-)
 #'
 #' @import data.table
 #' @import OBIC
 #'
 #' @export
-bln_wat_nretention_sw <- function(ID,B_LU_BRP,B_SOILTYPE_AGR,B_AER_CBS,B_GWL_CLASS,A_SOM_LOI,A_N_RT){
+bln_wat_nretention_sw <- function(ID,B_LU_BRP,B_SOILTYPE_AGR,B_AER_CBS,B_GWL_CLASS,A_SOM_LOI,A_N_RT,A_CN_FR = NA_real_){
 
   # add visual bindigns
-  bln_crops = code = choices = value_min = value_max = A_CN_FR = D_BDS = D_RD = D_OC = D_GA = FIELD_ID = D_NLV = D_NSW = NULL
+  bln_crops = code = choices = value_min = value_max = D_BDS = D_RD = D_OC = D_GA = FIELD_ID = D_NLV = D_NSW = NULL
 
   # make internal copy
   blnp <- BLN::bln_parms
@@ -52,6 +53,7 @@ bln_wat_nretention_sw <- function(ID,B_LU_BRP,B_SOILTYPE_AGR,B_AER_CBS,B_GWL_CLA
                    B_AER_CBS=B_AER_CBS,
                    A_SOM_LOI = A_SOM_LOI,
                    A_N_RT = A_N_RT,
+                   A_CN_FR = A_CN_FR,
                    value = NA_real_
   )
 
@@ -59,8 +61,9 @@ bln_wat_nretention_sw <- function(ID,B_LU_BRP,B_SOILTYPE_AGR,B_AER_CBS,B_GWL_CLA
   dt[, B_GWL_CLASS := OBIC::format_gwt(B_GWL_CLASS)]
   dt[, B_AER_CBS := OBIC::format_aer(B_AER_CBS)]
 
-  # add CN ratio
-  dt[, A_CN_FR := A_SOM_LOI *10 * 0.5 *1000/ A_N_RT]
+  # add CN ratio and run checks
+  dt[is.na(A_CN_FR), A_CN_FR := A_SOM_LOI *10 * 0.5 *1000/ A_N_RT]
+  checkmate::assert_numeric(dt$A_CN_FR, lower = blnp[code == "A_CN_FR", value_min], upper = blnp[code == "A_CN_FR", value_max])
 
   # calculate derivative supporting soil properties
   dt[, D_BDS := OBIC::calc_bulk_density(B_SOILTYPE_AGR,A_SOM_LOI)]
