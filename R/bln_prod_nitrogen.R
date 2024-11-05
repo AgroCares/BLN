cdb4c65678fbec54f6955ca58833afdbeb735e83
Gerard <gerard.ros@nmi-agro.nl>
2024-11-05 11:44:37 +0100

    add CN to bln_c_nitrogen

--- R/bln_prod_nitrogen.R
+++ R/bln_prod_nitrogen.R
@@ -6,6 +6,7 @@
 #' @param B_SOILTYPE_AGR (character) The agricultural type of soil
 #' @param A_SOM_LOI (numeric) The percentage organic matter in the soil
 #' @param A_N_RT (numeric) The organic nitrogen content of the soil in mg N / kg
+#' @param A_CN_FR (numeric) The carbon to nitrogen ratio (-)
 #'
 #' @import data.table
 #'
@@ -17,16 +18,17 @@
 #' The capacity of the soil to supply nitrogen (kg N / ha / yr). A numeric value, converted to a OSI score.
 #'
 #' @export
-bln_c_nitrogen <- function(B_LU_BRP, B_SOILTYPE_AGR,A_SOM_LOI,A_N_RT) {
+bln_c_nitrogen <- function(B_LU_BRP, B_SOILTYPE_AGR,A_SOM_LOI,A_N_RT,A_CN_FR = NA_real_) {
 
   # add visual bindings
   bln_country = bln_indicator = crop_code = crop_cat1 = bln_threshold_cropcat = NULL
-  A_CN_FR = D_BDS = D_RD = D_OC = D_GA = id = value = i_c_n = bln_st_c1 = NULL
+  D_BDS = D_RD = D_OC = D_GA = id = value = i_c_n = bln_st_c1 = NULL
 
   # Load in the crops data set and the parms dataset
   dt.crops <- as.data.table(BLN::bln_crops)
   dt.parms <- as.data.table(BLN::bln_parms)
   dt.thresholds <- as.data.table(BLN::bln_thresholds)
+  blnp <- BLN::bln_parms
 
   # subset thresholds to Dutch situation for phosphorus
   dt.thresholds <- dt.thresholds[bln_country=='NL' & bln_indicator=='i_c_n']
@@ -47,9 +49,13 @@ bln_c_nitrogen <- function(B_LU_BRP, B_SOILTYPE_AGR,A_SOM_LOI,A_N_RT) {
                    B_SOILTYPE_AGR = B_SOILTYPE_AGR,
                    A_SOM_LOI = A_SOM_LOI,
                    A_N_RT = A_N_RT,
-                   A_CN_FR = A_SOM_LOI * 10 * 0.5 * 1000/ A_N_RT,
+                   A_CN_FR = A_CN_FR,
                    value = NA_real_
                   )
+
+  # add CN ratio and run checks
+  dt[is.na(A_CN_FR), A_CN_FR := A_SOM_LOI *10 * 0.5 *1000/ A_N_RT]
+  checkmate::assert_numeric(dt$A_CN_FR, lower = blnp[code == "A_CN_FR", value_min], upper = blnp[code == "A_CN_FR", value_max])
 
   # merge with crop_category
   dt <- merge(dt, dt.crops[, list(crop_code, crop_cat1)],
